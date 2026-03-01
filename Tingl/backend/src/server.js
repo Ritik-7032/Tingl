622adb7057046d9870be2b1b5cbd233ae6b779c4
Ritik Kumar <singhritik7032@gmail.com>
2026-03-01 13:52:02 +0530

    error solved

--- Tingl/backend/src/server.js
+++ Tingl/backend/src/server.js
@@ -12,7 +12,9 @@ import { app, server } from "./lib/socket.js";
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
-
+console.log("Current dir:", __dirname);
+console.log("Static path:", path.join(__dirname, "../frontend/dist"));
+console.log("ENV:", ENV.NODE_ENV);
 app.use(express.json({ limit: "5mb" })); 
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
@@ -20,13 +22,18 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
+       app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-}
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
